9d47a54eb7af4514d37a7db0c50791b5eb4dd79a
Nakano Miku <mikoajin14@gmail.com>
2020-09-22 22:52:22 +0700

    with task1 day02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import './assets/style.css'
 import Login from './Login'
 import uuid from 'uuid/v4'
 import IndexAdmin from './Admin/'
 import IndexPedagang from './Pedagang/'
-import IndexUser from './User/'
+import IndexUser from './User/Index'
 import Logout from './Logout';
 
 
@@ -17,7 +17,6 @@ function App() {
     page: 1,  
     limit: 5
   })
-  const [stuff, setStuff] = useState([])
   const [user, setUser] = useState([
     {
       id: uuid(),
@@ -38,31 +37,18 @@ function App() {
       role: 3
     }
   ]) 
-  useEffect(() => {
-    
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-          let jsonpars = JSON.parse(xhr.response)
-          setStuff((prevStuff) => {
-            return ([...prevStuff], jsonpars)
-          })
-      }
-    }
-    xhr.open('GET', `https://jsonplaceholder.typicode.com/photos?_limit=${pagination.limit}&_start=${pagination.page}`)
-    xhr.send()
-  }, [pagination,isLogin,user])
-  console.log(pagination)
+
   let page = ""
       if (isLogin.role === 1) {
           page = <IndexAdmin setUser={setUser} user={user}/>
       } else if (isLogin.role === 2) {
           page = <IndexPedagang/>
       } else if (isLogin.role === 3) {
-          page = <IndexUser setPagination={setPagination} stuff={stuff}/>
+          page = <IndexUser pagination={pagination} setPagination={setPagination}/>
       }
   return (
     <div className="container">
+      {/* <IndexUser setPagination={setPagination} stuff={stuff}/> */}
         {isLogin.status === true ? <><Logout setIsLogin={setIsLogin}/>{page}</> : <Login user={user} setIsLogin={setIsLogin} />}
     </div>
   );
